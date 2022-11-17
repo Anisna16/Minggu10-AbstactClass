029e69cdf780dcfd4b58b12a0e4aef2ee6ef37ab
Anisna Hilwa Nadhifah <92202146+Anisna16@users.noreply.github.com>
2022-11-17 10:47:24 +0700

    Add files via upload

---
+++ MamaliaMain.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class MamaliaMain {
+    public static void main(String [] args){
+        Sapi sapi = new Sapi();
+        Paus paus = new Paus();
+        
+        sapi.deskripsi();
+        paus.deskripsi();
+    }
+}
